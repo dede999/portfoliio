e0bed7fe1a76bc84448cd684d033e82efa82129d
Andre Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-05-27 10:07:32 -0300

    Complete leptos application template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use leptos::*;
+
+#[component]
+fn App() -> impl IntoView {
+    view! {
+        <h1>"Hello, World!"</h1>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    _ = console_log::init_with_level(log::Level::Trace);
+    console_error_panic_hook::set_once();
+    mount_to_body(App)
 }
